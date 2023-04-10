b6c4ed707ebed37b512c1322c88dff360e53d938
ahussain61 <ahussain61@gatech.edu>
2023-04-09 21:45:36 -0400

    connected to the new Mongo database

--- back-end/db/conn.js
+++ back-end/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const uri = "mongodb+srv://coursemoreadmin:GT%21ChristMercyGrace%3B%3B23@cluster0.w5mecxi.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://coursemoreadmin:GT%21ChristMercyGrace%3B%3B23@cluster0.qdqpdp4.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(uri, {useNewUrlParser: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
